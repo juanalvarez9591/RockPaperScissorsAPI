b38ac9f9d3e5ec34db5075ec5d310cd98b978b13
Juan <Juan@DESKTOP-MFM86OU>
2022-02-04 19:49:36 -0300

    Add project files.

---
+++ LMADM/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LMADM.Models;
+
+namespace LMADM.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Move> Moves { get; set; }
+
+        public DbSet<GameLog> GameLogs { get; set; }
+
+    }
+}
---
+++ LMADM/Models/GameLog.cs
@@ -0,0 +1,13 @@
+﻿namespace LMADM.Models
+{
+    public class GameLog
+    {
+        public int Id { get; set; }
+        public string Player1 { get; set; } = String.Empty;
+        public string Player2 { get; set; } = String.Empty;
+        public string Winner { get; set; } = String.Empty;
+        public string Loser { get; set; } = String.Empty;
+        public int TotalRounds { get; set; }
+
+    }
+}
